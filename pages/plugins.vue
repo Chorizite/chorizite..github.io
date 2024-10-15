0c04e3662814b76c304c78e4fc00a2c6ae345ece
trevis <trevis@haxit.org>
2024-10-15 00:17:24 -0600

    add missing pages

---
+++ pages/plugins.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Plugins</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "headeronly",
+  documentDriven: false
+}) 
+</script>
+
+<style>
+
+</style>
